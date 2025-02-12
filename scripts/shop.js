0573a87597852a82578ea224ed9e6854ef64905f
Robin Björkil <robin.bjorkil@iths.se>
2025-02-12 21:39:48 +0100

    Added functions for remove, update price and clear cart button.
    Added functions for remove items from cart, update price and clear cart button.

--- scripts/shop.js
+++ scripts/shop.js
@@ -58,11 +58,11 @@ document.addEventListener("DOMContentLoaded", () =>{
         }
     ];
 
-    /* En variabel som får hålla produkterna */
+/* En variabel som får hålla produkterna */
     const productContainer = document.getElementById("products");
 
      
-    /* Här loopar jag igenom varje element i "products" och gör något med dessa element */
+/* Här loopar jag igenom varje element i "products" och gör något med dessa element */
     products.forEach((product, index) => {
 
         /* 1. Skapar en div-behållare för varje produkt
@@ -92,41 +92,51 @@ document.addEventListener("DOMContentLoaded", () =>{
         });
     });
 
-    /* Funktion för att lägga till och ta bort enskilda köp i kundvagnen "Cart" */
-        function addToCart(product){
+/* Funktion för att lägga till köp i kundvagnen "Cart" */
+    function addToCart(product){
 
-            /* 1. Variabel för att hålla reda på mina köp i en li "list item
-               2. Bootstrap för ett snyggare utseende*/
-            const cartItem = document.createElement("li");
-            cartItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+         /* 1. Variabel för att hålla reda på mina köp i en li "list item
+            2. Bootstrap för ett snyggare utseende*/
+        const cartItem = document.createElement("li");
+        cartItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
 
-            /* Gör en lista av mina köp med namn och pris och en röd X-knapp för att ta bort */
-            cartItem.innerHTML = `
-            ${product.name} - ${product.price} SEK
-            <button class="btn btn-danger btn-sm remove-from-cart">X</button>
-            `;
+        /* Gör en lista av mina köp med namn och pris och en röd X-knapp för att ta bort */
+        cartItem.innerHTML = `
+        ${product.name} - ${product.price} SEK
+        <button class="btn btn-danger btn-sm remove-from-cart">X</button>
+        `;
 
-            /* Lägger till produkten i kundvagnens lista på sidan, sparar den i arrayen cart och uppdaterar det totala priset. */
-            cartList.appendChild(cartItem);
-            cartItem.push(product);
-            updateTotal(product.price);
+        /* Lägger till produkten i kundvagnens lista på sidan, sparar den i arrayen cart och uppdaterar det totala priset. */
+        cartList.appendChild(cartItem);
+        cartItem.push(product);
+        updateTotal(product.price);
 
-            /* Eventlistener med click för X-knappen för att ta bort */
-            cartItem.querySelector(".remove-from-cart").addEventListener("click", () => {
-                removeFromCart(product, cartItem);
-            });  
-        }
+        /* Eventlistener med click för X-knappen för att ta bort */
+        cartItem.querySelector(".remove-from-cart").addEventListener("click", () => {
+            removeFromCart(product, cartItem);
+        });  
+    }
 
+/* Funktion för att ta bort enskilda rader med köp i kundvagn */
+    function removeFromCart(product, cartItem) {
+        cartList.removeChild(cartItem);
+        cart.splice(cart.indexOf(product), 1);
+        updateTotal(-product.price);
+    }
 
+/* Funktion för att uppdatera totalpriset i kundvagnen */
+    function updateTotal(amount) {
+        total += amount;
+        totalPriceElement.textContent = `Total: ${total} SEK`;
+    }
+
+/* Funktion för att tömma hela kundvagnen */
+    clearCartButton.addEventListener("click", () => {
+        cartList.innerHTML = "";
+        cart = [];
+        total = 0;
+        totalPriceElement.textContent = "Total 0 SEK";
+    });
     
-
-
-
-
-
-
-
-
-
 
 });
